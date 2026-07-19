6b6c9cfd50c2800156473a75503ee6cd33fabb2e
riderdevchan <34148113+riderdevchan@users.noreply.github.com>
2026-07-19 11:09:43 +0700

    Update service worker cache and event listeners

--- sw.js
+++ sw.js
@@ -1,22 +1,14 @@
-const CACHE_NAME = 'coinman-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'coinman-cache-v1';
+const urlsToCache = ['./index.html', './manifest.json', './icon.png'];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
